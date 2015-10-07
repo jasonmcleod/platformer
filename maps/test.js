7737c4e6c7d7ad0ad7cdc7247582872069e1b984
Jason McLeod <pixelparty@gmail.com>
2015-10-07 17:27:47 -0400

    initial commit

---
+++ maps/test.js
@@ -0,0 +1,21 @@
+var map = [
+    'WWWWWWWWWWWWWWWWWWWWWWWWW',
+    'W                       W',
+    'W                       W',
+    'W                       W',
+    'W                       W ',
+    'W                        ',
+    'W                        ',
+    'W                        ',
+    'W                        ',
+    'W                        ',
+    'W                       W',
+    'W                       W',
+    'W                       W',
+    'WWW                     W',
+    'W                       W',
+    'W        9              W',
+    'W                       W',
+    'WP                      W',
+    'WWWWWWWWWWWWWWWWWWWWWWWWW'
+];
